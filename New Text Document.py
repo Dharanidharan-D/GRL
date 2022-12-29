0e93efcadb984b8cf343cbcef7220c6121235391
Dharanidharan-D <105580415+Dharanidharan-D@users.noreply.github.com>
2022-12-29 11:04:46 +0530

    Update New Text Document.py
    updated

--- New Text Document.py
+++ New Text Document.py
@@ -1,21 +1,32 @@
-class main:
-    class dataset:
-        distance=int(input())
-        time=int(input())
-        speed=int(input())
+class Main:
     
-    class Userinput(dataset):
-        user=input()
-        user=user.split(",")
-        data=[]
-        for i in range(len(user)):
-            data.append(input())
-        
-    class Measure(Userinput):
-        data1=[]
-        for i in data:
-            data1.append(i[0]*i[1])
+    def dataset(self):
+        print('dataset')
+        self.distance=int(input())
+        self.time=int(input())
+        self.speed=int(input())
+
+    def Userinput(self):
+        print('userinput')
+        self.user=input()
+        self.user=self.user.split(",")
+        self.data=[]
+        for i in range(len(self.user)):
+            x=int(input())
+            y=int(input())
+            self.data.append([x,y])
+
+    def Measure(self):
+        self.data1=[]
+        for i in self.data:
+            self.data1.append(i[0]*i[1])
+
+    def Showresult(self):
+        for i in range(len(self.data1)):
+            print(self.user[i],self.data1[i])
             
-    class Showresult(Userinput):
-        for i in range(len(data1)):
-            print(user[i],data1[i])
+dharani=Main()
+dharani.dataset()
+dharani.Userinput()
+dharani.Measure()
+dharani.Showresult()
